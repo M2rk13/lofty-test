493b4c2f33247a3f969072f1fdee2eaec51037b6
Mark Morgunov <70945005+M2rk13@users.noreply.github.com>
2021-10-20 23:04:37 +0300

    Определены сущности и схема БД
    Создана миграция для БД, таблицы сотрудников и должностей.
    Созданы модели.
    Внесена корректировка в форму регистрации для активации записи без подтверждения email.

---
+++ www/lofty/frontend/models/Employee.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "employee".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $birthday
+ * @property string $hiring_date
+ * @property int $position_id
+ *
+ * @property Position $position
+ */
+class Employee extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'employee';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'hiring_date', 'position_id'], 'required'],
+            [['position_id'], 'integer'],
+            [['hiring_date', 'birthday'], 'safe'],
+            [['name'], 'string', 'max' => 255],
+            [['position_id'], 'exist', 'skipOnError' => true, 'targetClass' => Position::className(), 'targetAttribute' => ['position_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'birthday' => 'Birthday',
+            'hiring_date' => 'Hiring Date',
+            'position_id' => 'Position ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[Position]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPosition()
+    {
+        return $this->hasOne(Position::className(), ['id' => 'position_id']);
+    }
+}
